17dfc34c6a25d2a0dead196d5fc67dc09eb66b89
CaptainC4p <lukas.de-araujo@outlook.fr>
2025-05-06 23:54:56 +0200

    Update pialert.sh

--- pialert.sh
+++ pialert.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s https://github.com/CaptainC4p/test-promox-scripter-8.3.1/blob/main/build.func)
+source <(curl -s https://raw.githubusercontent.com/CaptainC4p/test-promox-scripter-8.3.1/refs/heads/main/build.func)
 # Copyright (c) 2021-2024 tteck
 # Author: tteck (tteckster)
 # License: MIT
